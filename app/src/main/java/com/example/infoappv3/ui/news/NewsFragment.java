015750bd2ecae257812eb94dc8e0b7eb749f4dea
Maximilian <maxel2804@gmail.com>
2020-08-13 22:05:33 +0200

    navBar

--- app/src/main/java/com/example/infoappv3/ui/news/NewsFragment.java
+++ app/src/main/java/com/example/infoappv3/ui/news/NewsFragment.java
@@ -14,15 +14,15 @@ import com.example.infoappv3.R;
 
 public class NewsFragment extends Fragment {
 
-  private HomeViewModel homeViewModel;
+  private NewsViewModel newsViewModel;
 
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState) {
-    homeViewModel =
-        ViewModelProviders.of(this).get(HomeViewModel.class);
+    newsViewModel =
+        ViewModelProviders.of(this).get(NewsViewModel.class);
     View root = inflater.inflate(R.layout.fragment_news, container, false);
     final TextView textView = root.findViewById(R.id.text_home);
-    homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+    newsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
       @Override
       public void onChanged(@Nullable String s) {
         textView.setText(s);
