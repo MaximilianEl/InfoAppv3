0abba501aeb5540c5e33ba4089105779d6e9eb31
Maximilian <maxel2804@gmail.com>
2020-08-13 18:23:04 +0200

    navBar

--- app/src/main/java/com/example/infoappv3/MainActivity.java
+++ app/src/main/java/com/example/infoappv3/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.infoappv3;
 
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+  Timer timer;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    timer = new Timer();
+    timer.schedule(new TimerTask() {
+      @Override
+      public void run() {
+        Intent intent = new Intent(MainActivity.this, NavActivity.class);
+        startActivity(intent);
+        finish();
+      }
+    }, 3000);
   }
 }
